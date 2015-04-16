661bb94060a46653c5d472db8ee0e33d9d5375dc
Evan Dorsky <evan.dorsky@icloud.com>
2015-04-16 16:07:51 -0400

    Remove broken things

--- arduino/main.ino
+++ arduino/main.ino
@@ -1,7 +1,6 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
-// #include <digitalWriteFast.h>
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *motor = AFMS.getMotor(1);
