5dff9770ebc933080535e010f088b04eef682bbf
Forrest Bourke <forrestbourke@gmail.com>
2015-04-16 14:47:01 -0400

    made two timers and an encoder reading thing plus a velocity calculating thing

--- arduino/motor-control.ino
+++ arduino/motor-control.ino
@@ -2,6 +2,10 @@
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
 
+//in hertz
+#define ENCODER_FREQ 10000
+#define VELOCITY_FREQ 20
+
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
 Adafruit_DCMotor *motor = AFMS.getMotor(1);
@@ -9,9 +13,14 @@ Adafruit_DCMotor *motor = AFMS.getMotor(1);
 int pot = A1;
 int A = 2;
 int B = 3;
-int encoderPos = 0;
-int encoderLast = LOW;
-int n = LOW;
+unsigned long encoderPos = 0;
+unsigned long encoderTimer = 0;
+unsigned long velocityTimer = 0;
+volatile int encoderLast = LOW;
+volatile int n = LOW;
+volatile int nLast = 0;
+volatile int velocity = 0;
+
 
 int dir = FORWARD;
 int error = 0;
@@ -24,6 +33,8 @@ static unsigned long switchtime = 0;
 void setup() {
   Serial.begin(9600);
   Serial.println("DC Motor test");
+  encoderTimer = micros();
+  velocityTimer = millis();
 
   AFMS.begin();
 }
@@ -40,17 +51,29 @@ void loop() {
   Serial.println(mspeed);
 
   //===== READING THE ENCODER ======
-  n = digitalRead(encoder0PinA);
-   if ((encoder0PinALast == LOW) && (n == HIGH)) {
-     if (digitalRead(encoder0PinB) == LOW) {
-       encoder0Pos--;
-     } else {
-       encoder0Pos++;
-     }
-     Serial.print (encoder0Pos);
-     Serial.print ("/");
-   } 
-   encoder0PinALast = n;
+  // from http://playground.arduino.cc/Main/RotaryEncoders#Example1
+  // doesn't use interrupts
+  if(micros() - encoderTimer > 1000000/ENCODER_FREQ)
+  {
+	  encoderTimer = micros();
+	  n = digitalRead(A);
+	   if ((encoderLast == LOW) && (n == HIGH)) {
+	     if (digitalRead(B) == LOW) {
+	       encoderPos--;
+	     } else {
+	       encoderPos++;
+	     }
+	   } 
+	   encoderLast = n;
+   }
+
+	if(millis() - velocityTimer > 1000/VELOCITY_FREQ)
+	{
+	  velocityTimer = millis();
+	  //in clicks per millisecond?
+	  velocity = (n - nLast)*VELOCITY_FREQ;
+	  nLast = n;
+    }
 
   // Serial.println(millis()); // time when measurement started
   // Serial.println(pulseIn(A, HIGH)); // high pulse length
