d0764db7c6504b68d59d12ef3a3291d0fc6d0dcd
Evan Dorsky <evan.dorsky@icloud.com>
2015-04-22 00:01:27 -0400

    Basic math, no compensator, unity feedback

--- simulate/sim.m
+++ simulate/sim.m
@@ -2,14 +2,44 @@ clc, clear all
 
 s = tf('s');
 
-Ke = .0111;
-taum = .0596;
+% pendulum tf
+g = 9.8; % m/sec^2
+l = 0.0936; % m
+tauL = sqrt(g/l); % sec
 
-M = tf(1/Ke/(1 + taum*s));
+G = -s^2/g/((tauL*s + 1)*(tauL*s - 1));
+
+%% series compensator tf
+
+K = 1;
+
+%% motor tf
+Ke = .0111; % V*sec/rad
+tauM = .0596; % sec
+
+M = 1/Ke/(1 + tauM*s); % Vout/Vin
+
+% velocity minor loop
+kV = 1;
+
+Mc = M/(s*(1 + M*kV)); % X/Vin 
+
+% position positive feedback
+kP = 1;
+
+Lminor = K*Mc*kP; % minor position loop TF
+KM = K*Mc;
+
+KMp = KM/(1 - kP*KM); % actual minor loop
+
+Lmajor = KMp*G; % total loop TF
+XoverTheta = KMp/(1 - G*KMp); % total TF
 
 figure(1)
 clf
-subplot(211);
-margin(M);
-subplot(212);
-step(M);
+bode(Lmajor)
+hold on
+% subplot(211)
+% margin(M)
+% subplot(212)
+% step(M)
